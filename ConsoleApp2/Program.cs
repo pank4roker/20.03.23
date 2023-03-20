dbf9894bf97a94a5817b98d59c99fb4a1772e6c8
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2023-03-20 10:53:56 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Отсортировать одномерный массив
+            //1-й способ
+            int[] array = { -8, 6, 0, 11, 4, -4, 3 };
+            //for(int i=0;i<array.Length-1;i++)
+            //{
+            //    for(int j=i+1;j<array.Length;j++)
+            //    {
+            //        if (array[i] > array[j])
+            //        {
+            //            int p = array[i]; array[i] = array[j]; array[j] = p;                 
+            //        }
+
+            //    }
+            //}
+            //for(int i=0;i<array.Length;i++)
+            //{
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+            //}
+            //foreach(int elem in array)
+            //{
+            //    Console.WriteLine($"{elem}");
+            //}
+            Array.Sort(array);
+            foreach(int elem in array)
+            {
+                Console.WriteLine($"{elem}");
+            }
+            Array.Reverse(array);
+            Console.ReadKey();
+        }
+    }
+}
